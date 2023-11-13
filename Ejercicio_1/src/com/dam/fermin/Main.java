af4af4165fe19d65f3f67e284a3e8f01f88449d3
femafo <femafo@xuqueralumnat.es>
2023-11-13 20:59:00 +0100

    Upload

---
+++ Ejercicio_1/src/com/dam/fermin/Main.java
@@ -0,0 +1,45 @@
+package com.dam.fermin;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Fermin\n");
+		
+		System.out.println("Lluis Vives 3, 717702077\n");
+		
+		System.out.println("Hello\tHolaa\n");
+		
+		System.out.println("Pain\tDolor\n");
+		
+		System.out.println("Man\nHombre\n\n");
+		
+		System.out.println("\t\tLUNES\t\tMARTES\t\tMIERCOLES\tJUEVES\t\tVIERNES\n");
+		System.out.println("8:00\t\tFol\t\tSi\t\tSi\t\tAng\t\tEd");
+		System.out.println("8:55\t\tProg\t\tBda\t\tSi\t\tProg\t\tLLMM\n");
+		System.out.println("PATIO\t\tPATIO\t\tPATIO\t\tPATIO\t\tPATIO\t\tPATIO\n");
+		System.out.println("10:10\t\tProg\t\tBda\t\tBda\t\tProg\t\tLLMM");
+		System.out.println("11:05\t\tProg\t\tProg\t\tBda\t\tFol\t\tFol\n");
+		System.out.println("PATIO\t\tPATIO\t\tPATIO\t\tPATIO\t\tPATIO\t\tPATIO\n");
+		System.out.println("12:20\t\tEd\t\tProg\t\tBda\t\tSi\t\tProg");
+		System.out.println("13:15\t\tAng\t\tEd\t\tLLMM\t\tSi\t\tAng\n\n");
+		
+		System.out.println("\t   *");
+		System.out.println("\t  ***");
+		System.out.println("\t *****");
+		System.out.println("\t*******\n");
+		
+		System.out.println("\t   *");
+		System.out.println("\t  * *");
+		System.out.println("\t *   *");
+		System.out.println("\t*******");
+		
+		
+		
+		
+		
+
+	}
+
+}
